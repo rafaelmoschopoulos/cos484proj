6238138fc5c8081ddba6bd90f53d004ea3e5c54b
Rafael Moschopoulos <raf.mos@princeton.edu>
2025-04-06 21:35:10 -0400

    init

---
+++ paper_scripts/test.sh
@@ -0,0 +1,1 @@
+python run.py --output_name main --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
