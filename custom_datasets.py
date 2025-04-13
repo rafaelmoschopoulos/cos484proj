570b56020f58ce3ae877756ac4066d968e12441a
Rafael Moschopoulos <raf.mos@princeton.edu>
2025-04-13 16:35:26 -0400

    added script to pre_download all datasets

--- custom_datasets.py
+++ custom_datasets.py
@@ -1,4 +1,5 @@
 import random
+import os
 import datasets
 
 SEPARATOR = '<<<SEP>>>'
@@ -9,7 +10,7 @@ DATASETS = ['writing', 'english', 'german', 'pubmed']
 
 def load_pubmed(cache_dir):
     data = datasets.load_dataset('pubmed_qa', 'pqa_labeled', split='train', cache_dir=cache_dir)
-    
+
     # combine question and long_answer
     data = [f'Question: {q} Answer:{SEPARATOR}{a}' for q, a in zip(data['question'], data['long_answer'])]
 
@@ -43,15 +44,52 @@ def process_spaces(story):
         '\\\'', '\'').replace(
         '\n ', '\n').strip()
 
+def download_writing_prompts():
+    """
+    Downloads the Kaggle writing prompts dataset into the ./data folder
+    if it is not already present.
+    """
+    data_folder = './data'
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+    kaggle_config_dir = os.getcwd()  # assumes kaggle.json is here
+    os.environ['KAGGLE_CONFIG_DIR'] = kaggle_config_dir
+
+    kaggle_json_path = os.path.join(kaggle_config_dir, 'kaggle.json')
+    if not os.path.exists(kaggle_json_path):
+        raise OSError(
+            f"Could not find kaggle.json in {kaggle_config_dir}. "
+            "Please ensure that your kaggle.json file is in this folder or copy it to ~/.kaggle."
+        )
+
+    # Automatically adjust file permissions to 600, if possible.
+    try:
+        os.chmod(kaggle_json_path, 0o600)
+    except Exception as e:
+        print(f"Warning: Unable to change permissions on {kaggle_json_path}. {e}")
+
+    from kaggle.api.kaggle_api_extended import KaggleApi
+    api = KaggleApi()
+    api.authenticate()
+
+    dataset_dir = os.path.join(data_folder, 'writingPrompts')
+    if not os.path.exists(dataset_dir):
+        print("Downloading ratthachat/writing-prompts dataset to", data_folder)
+        api.dataset_download_files('ratthachat/writing-prompts', path=data_folder, unzip=True)
+    else:
+        print("Dataset already downloaded at", dataset_dir)
 
 def load_writing(cache_dir=None):
     writing_path = 'data/writingPrompts'
-    
+    if not os.path.exists(writing_path):
+        print(f"Writing dataset not found at {writing_path}. Initiating download...")
+        download_writing_prompts()
     with open(f'{writing_path}/valid.wp_source', 'r') as f:
         prompts = f.readlines()
     with open(f'{writing_path}/valid.wp_target', 'r') as f:
         stories = f.readlines()
-    
+
     prompts = [process_prompt(prompt) for prompt in prompts]
     joined = [process_spaces(prompt + " " + story) for prompt, story in zip(prompts, stories)]
     filtered = [story for story in joined if 'nsfw' not in story and 'NSFW' not in story]
